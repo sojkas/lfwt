e62cf07dbc89c15c0229a8d98344b99740ecd649
Stepan Sojka <sojka.stepan@gmail.com>
2022-05-27 21:44:54 +0200

    uprava componenty DepotUnit

---
+++ src/components/DepotSetUnit.tsx
@@ -0,0 +1,84 @@
+import React, { useEffect, useState } from "react";
+import Grid from "@mui/material/Grid";
+import {
+  IconButton,
+  TextField,
+  InputLabel,
+  Select,
+  MenuItem,
+  SelectChangeEvent,
+  FormControl,
+} from "@mui/material";
+import { RemoveCircleOutline } from "@mui/icons-material";
+import { DepotUnit } from "../models/settings";
+
+const DepotSetUnit: React.FC<{
+  depotUnit: DepotUnit;
+  removeDepotUnit: (id: string) => void;
+  updateDepotUnit: (depotUnit: DepotUnit) => void;
+}> = (props) => {
+  const [depotUnit, setDepotUnit] = useState<DepotUnit>(props.depotUnit);
+
+  const depotSlotsHandler = (event: SelectChangeEvent) => {
+    event.preventDefault();
+    const newValue: number = parseInt(event.target.value);
+    setDepotUnit((prevSetDepotUnit) => ({
+      ...prevSetDepotUnit,
+      depotSlotNumber: newValue,
+    }));
+  };
+
+  const depotNameChangeHandler = (event: React.ChangeEvent<HTMLInputElement>) => {
+    event.preventDefault();
+    setDepotUnit((prevDepotUnit)=>({...prevDepotUnit, depotName: event.target.value}));
+  }
+
+  const removeDepotUnitHandler = () => {
+    props.removeDepotUnit(props.depotUnit.id);
+  };
+
+  useEffect (()=>{
+    props.updateDepotUnit(depotUnit);
+  }, [depotUnit]);
+
+  return (
+    <React.Fragment>
+      <Grid container direction="row" spacing={2} className="item-space">
+        <Grid item xs={3}>
+          <TextField
+            id="depot"
+            label="Depot set"
+            variant="outlined"
+            value={props.depotUnit.depotName}
+            size="small"
+            onChange={depotNameChangeHandler}
+          />
+        </Grid>
+        <Grid item xs={3}>
+          <FormControl fullWidth>
+            <InputLabel id="slot-depot-label">Depot slots</InputLabel>
+            <Select
+              labelId="slot-depot-label"
+              id="slot-depot-select"
+              value={props.depotUnit.depotSlotNumber.toString()}
+              label="Slots per Depot"
+              onChange={depotSlotsHandler}
+              size="small"
+            >
+              <MenuItem value={4}>4</MenuItem>
+              <MenuItem value={8}>8</MenuItem>
+              <MenuItem value={12}>12</MenuItem>
+            </Select>
+          </FormControl>
+        </Grid>
+        <Grid item xs={3}>
+          <IconButton aria-label="remove" onClick={removeDepotUnitHandler}>
+            <RemoveCircleOutline />
+          </IconButton>
+        </Grid>
+      </Grid>
+    </React.Fragment>
+  );
+};
+
+export default DepotSetUnit;
