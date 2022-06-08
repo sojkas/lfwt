77ba865503059750fc2e63c6bea3c3c017c857fc
Tomucha <tomucha@gmail.com>
2022-06-08 14:26:15 +0200

    Refaktor simulace.

--- src/components/Simulation.tsx
+++ src/components/Simulation.tsx
@@ -1,18 +1,9 @@
-import {
-  Paper,
-  TableBody,
-  TableCell,
-  TableContainer,
-  TableHead,
-  TableRow,
-  Button,
-  Stack,
-} from "@mui/material";
-import React, { useEffect, useState } from "react";
-import Order, { SimulationValues } from "../models/order";
+import {Button, Paper, Stack, TableBody, TableCell, TableContainer, TableHead, TableRow,} from "@mui/material";
+import React, {useEffect, useState} from "react";
+import {SimulationValues} from "../models/order";
 import Settings from "../models/settings";
 import generateOrders from "../utils/GenerateOrders";
-import { findCustomerById, findDistributionAreaById } from "../utils/supportFunctions";
+import {findCustomerById, findDistributionAreaById} from "../utils/supportFunctions";
 import {renderKeplerHtml} from "../utils/keplerUtils";
 
 let isStopped = true;
@@ -22,7 +13,7 @@ const Simulation: React.FC<{
   simulationValues: SimulationValues;
   updatedSimulationValues: (simulationValues: SimulationValues) => void;
 }> = (props) => {
-  const [orders, setOrders] = useState<Order[]>(props.simulationValues.orders);
+
   const [virtualClock, setVirtualClock] = useState<Date>(
       props.simulationValues.clockTime
   );
@@ -41,15 +32,18 @@ const Simulation: React.FC<{
     setTimeout(addRandomOrder, 500);
     setVirtualClock((prevClock) => {
       const newDate = addMinutes(prevClock, 15);
-      /* console.log("add ranodm -> generateOrders " + virtualClock?.toString()) */
-      setOrders((prevOrders) => {
-        const orders = generateOrders(props.settings, 1 / 4, newDate);
-        /* console.log("Generated orders: "+orders); */
-        return prevOrders.concat(orders);
-      });
       return newDate;
-    });
+    })
   };
+
+  useEffect(( )=> {
+    const newOrders = generateOrders(props.settings, 1 / 4, virtualClock);
+    props.updatedSimulationValues({
+      orders: [...props.simulationValues.orders,...newOrders],
+      isStopped: isStopped,
+      clockTime: virtualClock,
+    });
+  }, [virtualClock])
 
   const startHandler = () => {
     isStopped = false;
@@ -59,23 +53,27 @@ const Simulation: React.FC<{
   const stopHandler = () => {
     isStopped = true;
     props.updatedSimulationValues({
-      orders: orders,
+      orders: props.simulationValues.orders,
       isStopped: isStopped,
       clockTime: virtualClock,
     });
   };
 
   const deleteHandler = () => {
-    if (!isStopped) return;
-    setOrders([]);
-    setVirtualClock(new Date(Date.now()));
     isStopped = true;
+    const d = new Date(Date.now());
+    setVirtualClock(d);
+    props.updatedSimulationValues({
+      orders: props.simulationValues.orders,
+      isStopped: isStopped,
+      clockTime: d
+    });
   };
 
   const downloadHandler = () => {
     var dataStr =
         "data:text/json;charset=utf-8," +
-        encodeURIComponent(JSON.stringify(orders));
+        encodeURIComponent(JSON.stringify(props.simulationValues.orders));
     var dlAnchorElem = document.createElement("a");
     document.body.append(dlAnchorElem);
     dlAnchorElem.setAttribute("href", dataStr);
@@ -87,7 +85,7 @@ const Simulation: React.FC<{
   const keplerHandler = () => {
     var dataStr =
         "data:text/html;charset=utf-8," +
-        encodeURIComponent(renderKeplerHtml(orders));
+        encodeURIComponent(renderKeplerHtml(props.simulationValues.orders));
     var dlAnchorElem = document.createElement("a");
     document.body.append(dlAnchorElem);
     dlAnchorElem.setAttribute("href", dataStr);
@@ -95,22 +93,6 @@ const Simulation: React.FC<{
     dlAnchorElem.click();
     dlAnchorElem.remove();
   };
-
-  useEffect(() => {
-    props.updatedSimulationValues({
-      orders: orders,
-      isStopped: isStopped,
-      clockTime: virtualClock,
-    });
-    /* window.localStorage.setItem(
-      "orders",
-      JSON.stringify({
-        orders: orders,
-        isStopped: isStopped,
-        clockTime: virtualClock,
-      })
-    ); */
-  }, [orders, isStopped]);
 
   return (
       <div>
@@ -145,8 +127,8 @@ const Simulation: React.FC<{
               </TableRow>
             </TableHead>
             <TableBody>
-              {orders.length > 0 &&
-                  orders.map((order) => (
+              {props.simulationValues.orders.length > 0 &&
+                  props.simulationValues.orders.map((order) => (
                       <TableRow
                           key={order.id}
                           sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
